887a41f86003f6e79a016d1f1640db973e27c35f
tdrydendiaz <tdrydendiaz@gmail.com>
2019-07-30 16:12:02 +0100

    installs

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
-import React from 'react';
-
-import logo from './logo.svg';
+import React, { Component } from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStroopwafel } from "@fortawesome/free-solid-svg-icons";
 import './App.css';
+import { Button } from "reactstrap";
+import { faMugHot } from "@fortawesome/free-solid-svg-icons";
+import { faJava } from "@fortawesome/free-brands-svg-icons";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+library.add(faStroopwafel);
+library.add(faMugHot, faJava);
+
+
+
+
+
+
+
+// import logo from './logo.svg';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button
+          onClick={this.handleClick}
+          color="success" outline={true} disabled={false}>Success</Button>
+        <p>Favorite Food: <FontAwesomeIcon icon="stroopwafel" /></p>
+        <p>Favorite Drink: <FontAwesomeIcon icon="mug-hot" /></p>
+        <p> Also Coffee : <FontAwesomeIcon icon={["fab", "java"]} /></p>
+
+
+      </div>
+    );
+  }
 }
 
 export default App;
